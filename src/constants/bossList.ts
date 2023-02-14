912ac7a9a8a2342203aa64ce37cc8618ed6cf82e
lastedchan <lastedchan@gmail.com>
2023-02-15 00:36:22 +0900

    결정석 계산기 - 보스 데이터, UI 이미지 추가

---
+++ src/constants/bossList.ts
@@ -0,0 +1,41 @@
+export const bossList = [
+  "발록",
+  "자쿰",
+  "혼테일",
+  "힐라",
+  "피에르",
+  "반반",
+  "블러디퀸",
+  "벨룸",
+  "반 레온",
+  "아카이럼",
+  "매그너스",
+  "핑크빈",
+  "시그너스",
+  "스우",
+  "우르스",
+  "데미안",
+  "밸러드",
+  "란마루",
+  "노히메",
+  "루시드",
+  "크루스",
+  "카웅",
+  "파풀라투스",
+  "윌",
+  "진 힐라",
+  "검은 마법사",
+  "더스크",
+  "듄켈",
+  "선택받은 세렌",
+  "가디언 엔젤 슬라임",
+  "감시자 칼로스",
+];
+export const ORDER_BOSS = [14, 0, 1, 10, 3, 21, 22, 4, 5, 6, 7, 8, 2, 9, 11, 12, 13, 15, 29, 19, 23, 26, 24, 27, 25, 28, 30];
+export const ORDER_CLEARBOARD = [1, 10, 3, 21, 22, 4, 5, 6, 7, 8, 2, 9, 11, 12, 13, 15, 29, 19, 23, 26, 24, 27, 25, 28, 30];
+
+export const FILENAME_ICON = ".0.png";
+export const FILENAME_BTN = ".btBoss.normal.0.png";
+export const FILENAME_BTN_MOUSEOVER = ".btBoss.mouseOver.0.png";
+export const FILENAME_BTN_CHECKED = ".btBoss.checked.0.png";
+export const FILENAME_BACKGROUND = ".mob.png";
