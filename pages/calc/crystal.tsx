1419f4201bba318e05c1cbc7cff263eb06950dd6
lastedchan <lastedchan@gmail.com>
2023-02-22 15:04:03 +0900

    meta tag 설정

--- pages/calc/crystal.tsx
+++ pages/calc/crystal.tsx
@@ -6,12 +6,12 @@ export default function CrystalCalcIndex() {
     <>
       <Head>
         <title>LASTCHAN - 보스 결정 수익 계산기</title>
-        <meta name={"og:title"} content={"LASTCHAN - 보스 결정 수익 계산기"} />
+        <meta property={"og:title"} content={"LASTCHAN - 보스 결정 수익 계산기"} />
         <meta
-          name={"og:description"}
+          property={"og:description"}
           content={"주에 얼마를 버는지, 이번 주에 얼마를 벌었는지, 메이플스토리 보스 결정 수익을 쉽게 계산하고, 체크해보세요."}
         />
-        <meta name={"og:image"} content={"/crystal.png"} />
+        <meta property={"og:image"} content={"/crystal.png"} />
       </Head>
       <CrystalCalc />
     </>
