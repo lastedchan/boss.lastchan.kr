cc28db658810d62049537a81baf4bba20662afea
lastedchan <lastedchan@gmail.com>
2023-02-22 15:44:25 +0900

    meta tag 설정

--- src/constants/app.ts
+++ src/constants/app.ts
@@ -8,6 +8,6 @@ export const PAGES: Pages = [
     path: "/calc/crystal",
     title: "보스 결정 수익 계산기",
     description: "강렬한 힘의 결정석 판매 수익 계산기",
-    image: "/crystal.png",
+    image: "/images/crystal.png",
   },
 ];
