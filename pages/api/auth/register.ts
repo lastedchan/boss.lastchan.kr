fba0cbcbed3f1a9a734cca843d2a7ecd03beff6e
lastedchan <lastedchan@gmail.com>
2023-06-09 00:41:45 +0900

    remove api

--- pages/api/auth/register.ts
+++
@@ -1,6 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import { CharacterList } from "@/types/crystalCalc";
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse<CharacterList>) {
-  // req.
-}
--- pages/api/character/index.ts
+++
@@ -1,8 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import { get } from "@/libs/db";
-import { CharacterList } from "@/types/crystalCalc";
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse<CharacterList>) {
-  const data = await get<CharacterList>("select * from `character`");
-  res.json(data);
-}
