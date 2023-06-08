00304564b882da7e2351e5b788dba0178f1b8823
lastedchan <44193610+lastedchan@users.noreply.github.com>
2023-06-08 09:18:04 +0900

    bossName 크기 조정

--- src/components/organisms/bossPanel.tsx
+++ src/components/organisms/bossPanel.tsx
@@ -35,7 +35,7 @@ export default function BossPanel({ type }: Props) {
             flex: "0 0 auto",
             display: "grid",
             gridAutoFlow: "column",
-            gridTemplateColumns: "minmax(76px, 216px) minmax(84px, 1fr) 100px",
+            gridTemplateColumns: "minmax(36px, 216px) minmax(84px, 1fr) 100px",
           }}
         >
           <Typography pl={1}>보스</Typography>
